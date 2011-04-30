c6a7f65aecf0a1e0748083cf010b30075578fdd0
Nando Vieira <fnando.vieira@gmail.com>
2011-04-30 10:12:17 -0300

    Cleaning up dotfiles.

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 cp files/autotest ~/.autotest
-cp files/bash_completion.sh ~/.bash_completion.sh
 cp files/bash_profile ~/.bash_profile
 cp files/caprc ~/.caprc
-cp files/finder-path.applescript ~/.finder-path.applescript
-cp files/gem_completion.sh ~/.gem_completion.sh
-cp files/git_completion.sh ~/.git_completion.sh
 cp files/gitconfig ~/.gitconfig
 cp files/irbrc ~/.irbrc
 cp files/inputrc ~/.inputrc
@@ -14,12 +10,9 @@ cp files/rvmrc ~/.rvmrc
 cp files/gitignore ~/.gitignore
 
 mkdir ~/bin
-cp files/{grabbit,lyrics} ~/bin
-chmod +x ~/bin/grabbit
-chmod +x ~/bin/lyrics
 
-curl http://github.com/paulhammond/webkit2png/raw/master/webkit2png > ~/.bin/webkit2png
-chmod +x ~/bin/webkit2png
+curl http://github.com/paulhammond/webkit2png/raw/master/webkit2png > ~/.bash/bin/webkit2png
+chmod +x ~/.bash/bin/webkit2png
 
 mkdir -p ~/.ssh
 cp files/sshconfig ~/.ssh/config
