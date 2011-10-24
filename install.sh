21ebe455801533d7016e40ee0b4cf35da661eaaa
Willian Fernandes <willian@willianfernandes.com.br>
2011-10-24 11:02:32 -0200

    update my preferences

--- install.sh
+++ install.sh
@@ -1,26 +1,17 @@
-cp files/autotest ~/.autotest
-cp files/bash_completion.sh ~/.bash_completion.sh
 cp files/bash_profile ~/.bash_profile
-cp files/bashrc ~/.bashrc
 cp files/caprc ~/.caprc
-cp files/gem_completion.sh ~/.gem_completion.sh
-cp files/gemrc ~/.gemrc
-cp files/git_completion.sh ~/.git_completion.sh
 cp files/gitconfig ~/.gitconfig
-cp files/gitignore ~/.gitignore
-cp files/inputrc ~/.inputrc
 cp files/irbrc ~/.irbrc
+cp files/inputrc ~/.inputrc
+cp files/gemrc ~/.gemrc
 cp files/npmrc ~/.npmrc
-cp files/rdebugrc ~/.rdebugrc
-cp files/rvmrc ~/.rvmrc
+cp files/rvmrc ~/.pryrc
+cp files/gitignore ~/.gitignore
 
 mkdir ~/bin
-cp files/{grabbit,lyrics} ~/bin
-chmod +x ~/bin/grabbit
-chmod +x ~/bin/lyrics
 
-curl http://github.com/paulhammond/webkit2png/raw/master/webkit2png > ~/bin/webkit2png
-chmod +x ~/bin/webkit2png
+curl http://github.com/paulhammond/webkit2png/raw/master/webkit2png > ~/.bash/bin/webkit2png
+chmod +x ~/.bash/bin/webkit2png
 
 mkdir -p ~/.ssh
 cp files/sshconfig ~/.ssh/config
