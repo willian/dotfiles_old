9a99ea8d7031ea5518f43bebc99c8c8197b4dcb4
Willian Fernandes <willian@willianfernandes.com.br>
2012-09-04 09:55:46 -0300

    updated installation script

--- install.sh
+++ install.sh
@@ -1,26 +1,29 @@
-cp files/bash_profile ~/.bash_profile
-cp files/caprc ~/.caprc
-cp files/gitconfig ~/.gitconfig
-cp files/irbrc ~/.irbrc
-cp files/inputrc ~/.inputrc
-cp files/gemrc ~/.gemrc
-cp files/npmrc ~/.npmrc
-cp files/pryrc ~/.pryrc
-cp files/gitignore ~/.gitignore
+CURRENT_PATH=`pwd`
 
-mkdir -p ~/bin
+ln -sf $CURRENT_PATH/files/bash_profile $HOME/.bash_profile
+ln -sf $CURRENT_PATH/files/caprc $HOME/.caprc
+ln -sf $CURRENT_PATH/files/gitconfig $HOME/.gitconfig
+ln -sf $CURRENT_PATH/files/irbrc $HOME/.irbrc
+ln -sf $CURRENT_PATH/files/inputrc $HOME/.inputrc
+ln -sf $CURRENT_PATH/files/gemrc $HOME/.gemrc
+ln -sf $CURRENT_PATH/files/npmrc $HOME/.npmrc
+ln -sf $CURRENT_PATH/files/pryrc $HOME/.pryrc
+ln -sf $CURRENT_PATH/files/gitignore $HOME/.gitignore
 
-curl https://raw.github.com/paulhammond/webkit2png/master/webkit2png > ~/.bash/bin/webkit2png
-chmod +x ~/.bash/bin/webkit2png
+mkdir -p $HOME/bin
 
-mkdir -p ~/.ssh
-cp files/sshconfig ~/.ssh/config
+curl https://raw.github.com/paulhammond/webkit2png/master/webkit2png > $HOME/.bash/bin/webkit2png
+chmod +x $HOME/.bash/bin/webkit2png
+
+mkdir -p $HOME/.ssh
+cp files/sshconfig $HOME/.ssh/config
 
 sudo mkdir -p /etc/bash_completion
 
-source ~/.bash_profile
+source $HOME/.bash_profile
 
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
 
 open files/IR_Black.terminal
+
