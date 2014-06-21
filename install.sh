aef7d0024e8a67d5661459768530544c923f1999
Willian Fernandes <willian@willianfernandes.com.br>
2014-06-21 14:12:24 -0300

    Added tmux.conf and ansible.cfg files

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 CURRENT_PATH=`pwd`
 
 ln -sf $CURRENT_PATH/files/ackrc $HOME/.ackrc
+ln -sf $CURRENT_PATH/files/ansible.cfg $HOME/.ansible.cfg
 ln -sf $CURRENT_PATH/files/bash_profile $HOME/.bash_profile
 ln -sf $CURRENT_PATH/files/caprc $HOME/.caprc
 ln -sf $CURRENT_PATH/files/gemrc $HOME/.gemrc
@@ -10,6 +11,7 @@ ln -sf $CURRENT_PATH/files/inputrc $HOME/.inputrc
 ln -sf $CURRENT_PATH/files/irbrc $HOME/.irbrc
 ln -sf $CURRENT_PATH/files/npmrc $HOME/.npmrc
 ln -sf $CURRENT_PATH/files/pryrc $HOME/.pryrc
+ln -sf $CURRENT_PATH/files/tmux.conf $HOME/.tmux.conf
 
 mkdir -p $HOME/bin
 
@@ -25,4 +27,3 @@ source $HOME/.bash_profile
 
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
-
